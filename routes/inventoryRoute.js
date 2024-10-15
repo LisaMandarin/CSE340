4ee8d5d98e72fe2d06f9b51e1218fb0c83bff690
LisaMandarin <chu23001@byui.edu>
2024-10-15 20:42:56 +0800

    W05 Activity2-1 done

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -28,4 +28,9 @@ router.post(
     utilities.handleErrors(invController.addInventory)
 )
 
+
+router.get(
+    "/getInventory/:classification_id",
+    utilities.handleErrors(invController.getInventoryJSON)
+)
 module.exports = router;
